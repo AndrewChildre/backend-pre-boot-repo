1968dd3cb9b9eecabc19802330181173a0db93dd
andrew childre <andrewchildre@gmail.com>
2021-04-23 17:28:32 -0600

    file move

--- backend/README.md
+++
@@ -1,3 +0,0 @@
-# pre-boot-repo
-
-new repo other one bad
--- info/views.py
+++ info/views.py
@@ -4,5 +4,5 @@ from .serializers import InfoSerializers
 from .models import Info
 
 class InfoViewSet(viewsets.ModelViewSet):
-    serializer_class = InfoSerializers
     queryset = Info.objects.all()
+    serializer_class = InfoSerializers
